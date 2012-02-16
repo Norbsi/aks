6bc5b2880494702558cf2516bf0f03b23da1f060
Sebastian Reinhold <sebastian@bitbernd.de>
2012-02-16 15:17:29 +0100

    added move threshold to map

--- src/app/Map.java
+++ src/app/Map.java
@@ -13,11 +13,12 @@ public class Map extends JComponent {
 	 */
 	private static final long serialVersionUID = -8929711985144231275L;
 	private Controller 	controller;
-	private int			maxCamPos;
+	private int			maxCamPos, moveThreshold;	
 	
 	public Map(Controller controller) {
-		this.controller = controller;
-		this.maxCamPos 	= this.controller.getConfiguration().getMaxCamPos();
+		this.controller 	= controller;
+		this.maxCamPos 		= this.controller.getConfiguration().getMaxCamPos();
+		this.moveThreshold	= this.controller.getConfiguration().getMoveThreshold();
 	}
 	
     public void paint(Graphics gg) {
@@ -29,15 +30,16 @@ public class Map extends JComponent {
     	
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, width, height);
-		
-		g.setColor(Color.DARK_GRAY);
 				
 		if (this.controller.getCamState() != null) {
 		double camPos		= this.controller.getCamState().getCamPosX();
 		double camPosAngle	= camPos * 45 / this.maxCamPos;
 		
+		g.setColor(Color.DARK_GRAY);
 		// 90->start angle (north), 30->half field of view
 		g.fillArc(-width/2, -height/2, width*2, height*2, 90 + 30 - (int) camPosAngle, -60);
+		g.setColor(Color.GRAY);
+		g.fillArc(-width/2, -height/2, width*2, height*2, 90 + this.moveThreshold/2 - (int) camPosAngle, -this.moveThreshold);
 		
 		g.setColor(Color.YELLOW);
     	for (Body body : this.controller.getRoomState().getBodyList()) {
