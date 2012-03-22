05590f1653c1c04c2c947d83e65e37ff73c98327
Norbert <Norbert@141.89.47.100>
2012-03-22 15:41:40 +0100

    Merge

--- src/serialPort/CamSerialConnection.java
+++ src/serialPort/CamSerialConnection.java
@@ -15,7 +15,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.TooManyListenersException;
 
-import app.Controller;
+import application.Controller;
 
 /**
 A class that handles the details of a serial connection. Reads from one 
